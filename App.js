be68d2158eb167c8519d17b42bb517aeb07f0f8a
dayaris rojas <programador1@qaecuador.com>
2021-05-26 14:45:37 -0500

    add primeros componentes

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import ModalApp from './modal';
+
 
 export default function App() {
+  const [suma, setSuma ] = useState(0);
+
+  const getSuma = () => {
+    setSuma(suma+1);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hola! {suma}</Text>
       <StatusBar style="auto" />
+
+      <Button
+        onPress={getSuma}
+        title="Contar"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
+
+      <ModalApp />
     </View>
   );
 }
